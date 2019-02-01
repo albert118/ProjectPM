463d6584d6f908825c044e715f3fe6aee2bfe7a1
theQuantumMechanic <albertferguson118@gmail.com>
2019-02-01 14:00:10 +1100

    Added custom about images for testing
    Only the HTML src requirements are added in this commit. The actual files are not to be synced to the repo and will be moved to their eventual storage positions in /var/www/static/...

--- src/wwwProjectPM/urls.py
+++ src/wwwProjectPM/urls.py
@@ -11,7 +11,7 @@ from . import views
 
 # Personalized admin site settings like title and header
 admin.site.site_title = "wwwprojectpm Site Admin"
-admin.site.site_header = "Wwwprojectpm Administration"
+admin.site.site_header = "wwwprojectpm Administration"
 
 urlpatterns = [
     path("", views.HomePage.as_view(), name="home"),
@@ -24,7 +24,6 @@ urlpatterns = [
     
 # User-uploaded files like profile pics need to be served in development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 # Include django debug toolbar if DEBUG is on
 if settings.DEBUG:
     import debug_toolbar
