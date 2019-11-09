6eb5c9d97eea4e5ff0f57f2ff79c3d77f3df3740
theQuantumMechanic <albertferguson118@gmail.com>
2019-11-10 10:09:52 +1100

    file management
    File management for virtualenv

---
+++ webproj/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\alber\onedrive\documents\github\projectpm\webproj\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
